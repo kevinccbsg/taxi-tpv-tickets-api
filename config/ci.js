412f46766972c164a2f36f71968cccd19195aaab
kevin Martínez <kevinccbsg@gmail.com>
2020-01-14 17:01:57 +0100

    Add circleci integration

---
+++ config/ci.js
@@ -0,0 +1,6 @@
+module.exports = {
+	mongo: {
+		connectionString: 'mongodb://localhost:27017/tpv-tickets',
+		contentDatabaseName: 'tpv-tickets',
+	},
+};
--- config/default.js
+++ config/default.js
@@ -36,7 +36,7 @@ module.exports = {
 		},
 	},
 	mongo: {
-		connectionString: process.env.DB_CONNECTION_STRING || 'mongodb://node:node@localhost:27017/tpv-tickets',
+		connectionString: process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/tpv-tickets',
 		contentDatabaseName: process.env.DATABASE_NAME || 'tpv-tickets',
 	},
 	logger: {
